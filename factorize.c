9bd7fcd71150df0e2687f71bc9b74537db37494c
alibayomy <alibayomy12@gmail.com>
2023-10-02 12:40:51 -0700

    adjusting the file

--- factorize.c
+++ factorize.c
@@ -4,20 +4,21 @@
  * @input: the given number as int
  * Return: 0 on success, 1 else
 */
-int factorize(unsigned   long int number)
+int factorize(unsigned  long int number)
 {
-	int prime_num[] = {2, 3, 5, 7 ,11, 13, 17, 19, 23, 29, 31,
+	/* int prime_num[] = {2, 3, 5, 7 ,11, 13, 17, 19, 23, 29, 31,
 						37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
-						79, 83, 89, 97};
-	int i;
+						79, 83, 89, 97}; */
+	int i = 2;
 
-	for (i = 0; prime_num[i] < 100; i++)
+	while (1)
 	{
-		if ((number % prime_num[i]) == 0 )
+		if ((number % i) == 0 )
 		{
-			printf("%lu=%lu*%d\n", number, number / prime_num[i], prime_num[i]);
+			printf("%lu=%lu*%d\n", number, number / i, i);
 			return (0);
 		}
+		i++;
 	}
 	return (1);
 }
